8b33959ae425e257cad208ff05b56366146e79a7
W Travis Jones <wtravisjones@gmail.com>
2012-11-17 17:53:58 -0600

    Moved unit component to new file.

--- game.js
+++ game.js
@@ -13,32 +13,6 @@ $(document).ready(function() {
   Crafty.init(640,480);
   Crafty.canvas.init();
 
-  Crafty.c("Derp", {
-    init: function() {
-      this.addComponent("2D, Canvas, Tween, AI, unitAlive");
-         //.attr({x:63, y:63})
-      this.bind("TweenEnd", function() {
-            //console.log("TweenEnd " + this);
-            Crafty.trigger("DerpTweenEnd");
-      });
-
-      var self = this;
-      this.bind("Move", function() {
-        if (self === selectedDerp) {
-          selectedBox.x = self.x;
-          selectedBox.y = self.y;
-        }
-      });
-
-      this.dest = {x: 0, y: 0};
-      this.tilePos = {x: 0, y: 0};
-      this.status = '';
-      this.lastStatus = '';
-      this.lastLastStatus = '';
-    }
-  });
-
-
   Crafty.scene("main",function() {
     Crafty.background("#000");
 
